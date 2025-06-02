7168047a07279a46d2ab2e254950c0dbf57e5b18
DeadAngle-DDAGL007 <mansouri@gmx.fr>
2025-06-02 20:43:35 +0200

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,1 +1,19 @@
-// Authentication logic
+import NextAuth from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
+import { prisma } from './db'
+import { compare } from 'bcryptjs'
+
+export const { handlers, auth } = NextAuth({
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({ 
+          where: { email: credentials.email } 
+        })
+        return user && await compare(credentials.password, user.password) 
+          ? { id: user.id, email: user.email } 
+          : null
+      }
+    })
+  ]
+})// Authentication logic
