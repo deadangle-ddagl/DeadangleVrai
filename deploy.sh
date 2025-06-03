5e4b0bd8204e5da9a9f278630d9f5acf3b698dc3
deadangle-ddagl <mansouri.btp@gmail.com>
2025-06-03 18:36:46 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-npm run build && npx prisma migrate deploy && npm start
+
+echo "⚙️ Build de l'application..."
+npm run build
+
+echo "📦 Déploiement des migrations Prisma..."
+npx prisma migrate deploy
+
+echo "🚀 Lancement du serveur..."
+npm start
