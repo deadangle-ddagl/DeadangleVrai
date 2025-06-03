cfe2daa15adc37beb22db6b9ee48db3dc96f0703
deadangle-ddagl <mansouri.btp@gmail.com>
2025-06-03 19:49:14 +0100

    Create options.ts

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,38 @@
+import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { compare } from "bcryptjs";
+import prisma from "@/lib/prisma";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Mot de passe", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) return null;
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user || !user.password) return null;
+
+        const isValid = await compare(credentials.password, user.password);
+        if (!isValid) return null;
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/(auth)/_auth_/login",
+  },
+};
