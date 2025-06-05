d1070139f798530d9cdb5ec6d55e95488ecb5de9
deadangle-ddagl <mansouri.btp@gmail.com>
2025-06-05 16:34:10 +0100

    Create auth.ts

---
+++ lib/auth.ts
@@ -0,0 +1,12 @@
+'use server'
+import { supabase } from './supabase'
+
+export async function getSession() {
+  const { data: { session } } = await supabase.auth.getSession()
+  return session
+}
+
+export async function getUser() {
+  const { data: { user } } = await supabase.auth.getUser()
+  return user
+}
