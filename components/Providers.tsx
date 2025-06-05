7881f4f8eef3ac2c7345de457c554c3823e2bee3
deadangle-ddagl <mansouri.btp@gmail.com>
2025-06-05 16:35:18 +0100

    Update Providers.tsx

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,12 +1,13 @@
-'use client';
-
-import { SessionContextProvider } from '@supabase/auth-helpers-react';
-import { supabase } from '@/lib/supabase';
+'use client'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const supabase = createClientComponentClient()
+
   return (
     <SessionContextProvider supabaseClient={supabase}>
       {children}
     </SessionContextProvider>
-  );
+  )
 }
