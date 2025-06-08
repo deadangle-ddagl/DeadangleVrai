214c353de572ab3275317a87c04e5407a8f03d85
deadangle-ddagl <mansouri.btp@gmail.com>
2025-06-08 16:58:29 +0100

    Update Providers.tsx

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,9 +1,11 @@
-// components/Providers.tsx
 'use client';
 
-import { SessionProvider } from 'next-auth/react';
 import { ReactNode } from 'react';
 
-export function Providers({ children }: { children: ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+type Props = {
+  children: ReactNode;
+};
+
+export function Providers({ children }: Props) {
+  return <>{children}</>;
 }
