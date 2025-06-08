342f9cfad39903d211c82f0f381cc4adaed40b7b
deadangle-ddagl <mansouri.btp@gmail.com>
2025-06-08 17:06:24 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import './styles/globals.css';
 import { Providers } from '@/components/Providers';
+import type { Metadata } from 'next';
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'DEAD ANGLE',
   description: 'Marketplace fun & absurde',
 };
 
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
     <html lang="fr">
       <body>
         <Providers>{children}</Providers>
